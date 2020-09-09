b0f332a0e3aeedd2f8ef053e6f023c6b064e2421
gohan <lewinskiptr@gmail.com>
2020-09-09 21:46:04 +0200

    login component, views handled by router

--- Frontend/clonello/src/App.vue
+++ Frontend/clonello/src/App.vue
@@ -1,75 +1,23 @@
 <template>
-  <div id="app">
-    <topheader></topheader>
-    <projectheader></projectheader>
-
-    <div class="row">
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-        <maincard></maincard>
-    </div>
-    <router-view />
-  </div>
+  
+    <router-view ></router-view>
+  
 </template>
 
 <script>
-import TopHeader from "@/components/TopHeader.vue";
-import ProjectHeader from "@/components/ProjectHeader.vue";
-import MainCard from "@/components/MainCard.vue";
+  import Home from "../src/views/Home"
+  import Login from "../src/views/Login"
 
-export default {
-  components: {
-    "topheader": TopHeader,
-    "projectheader": ProjectHeader,
-    "maincard": MainCard
+
+  export default {
+    views: {
+      'home': Home,
+      'login': Login
+    }
   }
-};
+
 </script>
 
 <style lang="scss">
-
-body {
-    background-image: url("../src/assets/running-track-sn-3000x2001.jpg");
-    background-size: cover;
-    background-position: center;
-}
-
-.dropdown-header {
-    font-size: larger;
-    font-weight: bolder;
-}
-
-html, body {
-    height: 100%;
-}
-
-.list-head {
-    font-weight: bolder;
-    text-transform: uppercase;
-}
-
-.row {
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-  margin-left: -5px;
-  margin-right: -5px;
-  margin-top: 10px;
-  height: 100vh;
-}
-
-::-webkit-scrollbar {
-  height: 4px;              /* height of horizontal scrollbar ← You're missing this */
-  width: 4px;               /* width of vertical scrollbar */
-  border: 1px solid #d5d5d5;
-  margin-left: -5px;
-  margin-right: -5px;
-}
-
 
 </style>
---
+++ Frontend/clonello/src/views/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="login">
+        <form>
+            <h3 style="text-align: center">Sign In</h3>
+
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control form-control-lg" placeholder="name@example.com" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+
+            <button type="button" class="signin btn btn-dark btn-lg btn-block">
+                <router-link to="/home" exact>Sign In</router-link>
+            </button>
+
+            <button type="button" class="signup btn btn-primary">
+                <router-link to="/sign-up" exact>Sign up</router-link>
+            </button>
+            
+            <button type="button" class="forgot-password btn btn-primary">
+                <router-link to="/forgot-password" exact>Forgot password?</router-link>
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+body {
+    background-image: url("../assets/running-track-sn-3000x2001.jpg");
+    background-size: cover;
+    background-position: center;
+}
+
+html, body {
+    height: 100%;
+}
+
+
+.login {
+  width: 30%;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 12%;
+}
+
+label {
+  font-weight: 500;
+}
+
+.signup {
+    float: right;
+}
+
+.forgot-password,
+.forgot-password a,
+.signup {
+  text-align: center;
+  font-size: 13px;
+  color: #7a7a7a;
+  margin-top: 10px;
+}
+
+.forgot-password a,
+.signup a,
+.sihnin a {
+  color: #ffffff;
+}
+
+
+</style>
